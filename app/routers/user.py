e5460dacd12981c99603b8bf95da342c1eb45e69
Eduardo Davi <davibgr192@gmail.com>
2026-07-30 15:29:52 -0300

    feat: adiciona criacao de usuarios e hash de senha

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,3 +1,4 @@
+import bcrypt
 from app.schemas.user import UserCreate, UserResponse
 from app.database import get_db
 from app.models.user import User
@@ -7,10 +8,11 @@ router = APIRouter()
 
 @router.post("/usuarios", response_model=UserResponse)
 def criar_usuario(user: UserCreate, db = Depends (get_db)):
+    senha_hash = bcrypt.hashpw(user.senha.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
     novo_usuario = User(
     nome=user.nome,
     email=user.email,
-    senha=user.senha,
+    senha=senha_hash,
     tipo=user.tipo
 )
     db.add(novo_usuario)
